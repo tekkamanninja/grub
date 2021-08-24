912a5b3d8872daf0f596bf4bc11731a3271c0a33
Wesley PA4WDH <pa4wdh@yahoo.com>
2021-08-24 13:55:50 +0800

    riscv64: simply add the /chosen/boot-hartid node into the devicetree

--- grub-core/loader/efi/linux.c
+++ grub-core/loader/efi/linux.c
@@ -54,6 +54,11 @@ static int initrd_use_loadfile2;
 static grub_efi_guid_t load_file2_guid = GRUB_EFI_LOAD_FILE2_PROTOCOL_GUID;
 static grub_efi_guid_t device_path_guid = GRUB_EFI_DEVICE_PATH_GUID;
 
+#ifndef get_hartid
+#define get_hartid(__v)
+#warning 'missing get_hartid definition! please implement it in include/grub/cpu/linux.h'
+#endif
+
 grub_err_t
 grub_arch_efi_linux_check_image (struct linux_arch_kernel_header * lh)
 {
@@ -95,21 +100,25 @@ finalize_params_linux (void)
 
   void *fdt;
 
+  int hartid = 0;
+  get_hartid(hartid);
+
+  fdt = grub_fdt_load (GRUB_EFI_LINUX_FDT_EXTRA_SPACE);
+  if (!fdt)
+    goto failure;
+
+  node = grub_fdt_find_subnode (fdt, 0, "chosen");
+  if (node < 0)
+    node = grub_fdt_add_subnode (fdt, 0, "chosen");
+
+  if (node < 1)
+    goto failure;
+
+  grub_fdt_set_prop32 (fdt, node, "boot-hartid", hartid);
+
   /* Set initrd info */
   if (initrd_start && initrd_end > initrd_start)
     {
-      fdt = grub_fdt_load (GRUB_EFI_LINUX_FDT_EXTRA_SPACE);
-
-      if (!fdt)
-       goto failure;
-
-      node = grub_fdt_find_subnode (fdt, 0, "chosen");
-      if (node < 0)
-       node = grub_fdt_add_subnode (fdt, 0, "chosen");
-
-      if (node < 1)
-       goto failure;
-
       grub_dprintf ("linux", "Initrd @ %p-%p\n",
 		    (void *) initrd_start, (void *) initrd_end);
 
